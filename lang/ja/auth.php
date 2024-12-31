0ff5fc103238c0a857211d82834bd0d84448fd7f
masashi <no5858wish@gmail.com>
2024-12-31 16:02:41 +0900

    初期設定

---
+++ lang/ja/auth.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    // 'failed' => 'These credentials do not match our records.',
+    'failed' => 'ログイン情報が登録されていません。',
+    // 'password' => 'The provided password is incorrect.',
+    'password' => '入力されたパスワードが正しくありません。',
+    // 'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'throttle' => 'ログイン試行回数が多すぎます。:seconds 秒後にもう一度お試しください。',
+
+];
